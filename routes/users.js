c436f2debed9016682b11eaf75517c8bb3c0b1a8
billy-zeng <bzeng95@gmail.com>
2020-03-03 11:18:02 -0800

    cleans up line spacing

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/:id', ctrl.users.show);
-router.get('/profile/view', ctrl.users.show);
+// router.get('/profile/view', ctrl.users.show);
 router.post('/profile/edit', ctrl.users.edit);
 
 module.exports = router;
