f9f82ab037c3b10b9bdf5f62f43ee854456fb933
gpastorino <gpastorino@gmail.com>
2020-03-03 11:12:41 -0800

    auth, users, server changes

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,10 @@ router.get('/:id', ctrl.users.show);
 
 // url link to PROFILE page
 
-router.get('/profile/view', ctrl.users.show);
+// router.get('/profile/view', ctrl.users.show);
 
 router.post('/profile/edit', ctrl.users.edit);
+
 
 
 module.exports = router;
