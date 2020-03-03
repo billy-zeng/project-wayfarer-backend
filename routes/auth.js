02087c974cdd61dd6ba139fdd7f52da011e08c8f
billy-zeng <bzeng95@gmail.com>
2020-03-02 16:15:57 -0800

    resolves merge conflicts

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,8 @@ const ctrl = require('../controllers');
 
 //base route:  /api/v1/auth
 
-
 router.post('/signup', ctrl.auth.signup);
 router.post('/login', ctrl.auth.login);
-
-
-
-
 router.get('/verify', ctrl.auth.verify);
 router.delete('/logout', ctrl.auth.logout);
 
--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-
 router.get('/:id', ctrl.users.show);
-
-
-
-// url link to PROFILE page
-
 router.get('/profile/view', ctrl.users.show);
-
 router.post('/profile/edit', ctrl.users.edit);
-
 
 module.exports = router;
 
